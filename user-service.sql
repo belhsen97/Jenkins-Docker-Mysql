46461ddfea6babbf5fabb1e89bdfc3020f9600bb
Belhsen Bachouch <37088412+belhsen97@users.noreply.github.com>
2023-10-05 02:03:56 +0100

    Create user-service.sql

---
+++ user-service.sql
@@ -0,0 +1,4 @@
+USE `user-service`;
+INSERT INTO `account` ( `address`, `cin`, `city`, `created_at`, `date_of_birth`, `email`, `firstname`, `gender`, `lastname`, `linked_in`, `phone`, `state_region`,
+`zip_code`, `user_id`, `github`) VALUES ( 'rue tunis', 10820305, 'cite fahs', '2023-08-25 15:37:21', '1996-06-18', 'belhsenbachouch97@gmail.com', 'bachouch', 'MALE', 'belhsen', 
+'https://www.linkedin.com/in/belhsen-bachouch-2a0a72135/', 55775085, 'Zaghouan', 1140, 2, NULL);
